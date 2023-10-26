cdf1854bb6f5cc7cd66e98136bff240a3f78b2b1
unknown <d4dharmen>
2023-10-26 23:05:32 +0530

    try random

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/kaun-banega-crorepati-game=1",
+  base:"/kaun-banega-crorepati-game=1/",
   plugins: [react()],
 })
